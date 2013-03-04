5d1eed5b7342de86bd793b2932f78bfdf2e39bb4
viennadd <viennadd@gmail.com>
2013-03-04 17:44:36 +0800

    modification in NMatrix_Adjugate

--- NMatrix/NMatrix/NMatrix.c
+++ NMatrix/NMatrix/NMatrix.c
@@ -260,7 +260,7 @@ NMatrix* NMatrix_Adjugate(const NMatrix* mat)
        {
 		   for(i=0;i<dim;i++)
 		   {
-			   minor = NMatrix_Minor(mat,i,j,dim-1);
+			   minor = NMatrix_Minor(mat,j,i,dim-1);
 			   det = NMatrix_Determinant(minor,dim-1);
 			   adjugate->data[i][j] = pow(-1.0f,i+j+2.0f) * det;
 		   }
@@ -271,21 +271,65 @@ NMatrix* NMatrix_Adjugate(const NMatrix* mat)
 
 NMatrix* NMatrix_Inverse(const NMatrix* mat)
 {
+    
     NMatrix* inv = NULL;
     real det = 0.0f;
     real coeficent = 0.0f;
     if(mat->rows==mat->columns)
     {
+        
         det = NMatrix_Determinant(mat,mat->rows);
         if(det != 0.0f)
         {
         	inv = NMatrix_Create(mat->rows,mat->columns);
-        	coeficent = 1.0f/NMatrix_Determinant(mat,mat->rows);
+        	coeficent = 1.0f/det;
             inv = NMatrix_Adjugate(mat);
             inv = NMatrix_MultiplyWithScalar(inv,coeficent);
         }
     }
     return inv;
+
+    /*
+    double **matrix, ratio,a;
+    int i, j, k, n;
+    
+    matrix = mat->data;
+    n = mat->rows;
+
+    for(i = 0; i < n; i++){
+        for(j = n; j < 2*n; j++){
+            if(i==(j-n))
+                matrix[i][j] = 1.0;
+            else
+                matrix[i][j] = 0.0;
+        }
+    }
+    for(i = 0; i < n; i++){
+        for(j = 0; j < n; j++){
+            if(i!=j){
+                ratio = matrix[j][i]/matrix[i][i];
+                for(k = 0; k < 2*n; k++){
+                    matrix[j][k] -= ratio * matrix[i][k];
+                }
+            }
+        }
+    }
+    for(i = 0; i < n; i++){
+        a = matrix[i][i];
+        for(j = 0; j < 2*n; j++){
+            matrix[i][j] /= a;
+        }
+    }
+    printf("The inverse matrix is: \n");
+    for(i = 0; i < n; i++){
+        for(j = n; j < 2*n; j++){
+            printf("%.2f", matrix[i][j]);
+            printf("\t");
+        }
+        printf("\n");
+    }
+    return mat;*/
+
 }
 
 real NMatrix_MinElement(const NMatrix* mat)
